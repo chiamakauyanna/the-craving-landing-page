3e5fa27621c601b466a9baf35c7b52402e5489ff
Chiamaka Uyanna <chiamakauyanna@gmail.com>
2026-02-27 10:52:49 +0100

    Scaffolded my landing page

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      iuhiuhfewiuhd3ihdi ckjnishoidj3dejlkmlkwqniushoiwjlkxnsuiduihoij2 mw zijoi2joel2k
+    </div>
+  )
+}
+
+export default App
